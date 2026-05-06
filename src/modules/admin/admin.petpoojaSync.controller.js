4d16a4aeab34c150c914b3bed12696c189aec96e
DevVish12 <vishalrajput15817@gmail.com>
2026-05-06 19:03:57 +0530

    add more

--- src/modules/admin/admin.petpoojaSync.controller.js
+++ src/modules/admin/admin.petpoojaSync.controller.js
@@ -1,21 +1,4 @@
-// import { syncPetpoojaMenu } from "../petpooja/petpooja.sync.service.js";
 
-// export const syncPetpoojaMenuController = async (req, res) => {
-//     try {
-//         await syncPetpoojaMenu();
-//         return res.status(200).json({
-//             success: true,
-//             message: "Petpooja menu synced successfully",
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             success: false,
-//             message: error?.message || "Petpooja menu sync failed",
-//         });
-//     }
-// };
-
-// ✅ PUSH MODE - NO MANUAL SYNC
 
 export const syncPetpoojaMenuController = async (req, res) => {
     try {
